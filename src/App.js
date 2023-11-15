b37b6ca146955ff2a1ec8478a92ceed5b407763b
Storm Valet <valetviscount@gmail.com>
2023-11-15 23:57:21 +0530

    Created CSS
    Added the css and svg file for this project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
-import React from "react";
+import {useEffect} from "react";
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=7c4b4e2d'
+
+const movie1 = {
+    "Title": "Superman, Spiderman or Batman",
+    "Year": "2011",
+    "imdbID": "tt2084949",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+  }
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+
+    }
+
+    useEffect(() =>{
+        searchMovies('Spiderman');
+    },[]);
+
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="search for movie"
+                    value="Supermap"
+                    onChange = {() => {}}
+                />
+                <img 
+                src="{SearchIcon}"
+                alt="search"
+                onClick = {() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.year}</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
